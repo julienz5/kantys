b0e29cf8ffc53ded65e2084dc0790ce53a69faea
julienz5 <julien.zana5@gmail.com>
2016-02-16 10:28:06 +0100

    a

---
+++ global.js
@@ -0,0 +1,57 @@
+
+$(document).ready(function(){
+
+console.log("hello")
+
+  //.........................................................................Smooth Scroll
+
+ 	$('.scrollTo').click( function() { // Au clic sur un élément
+ 	var page = $(this).attr('href'); // Page cible
+ 	var speed = 750; // Durée de l'animation (en ms)
+ 	$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+ 	return false;
+ 	});
+
+ //.........................................................................Back to Top
+
+if ($('#backtotop').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#backtotop').addClass('show');
+            } else {
+                $('#backtotop').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#backtotop').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
+ //.........................................................................JQuery NEWS
+
+        $(".demo1").bootstrapNews({
+            newsPerPage: 3,
+            autoplay: true,
+			pauseOnHover:true,
+            direction: 'up',
+            newsTickerInterval: 4000,
+            onToDo: function () {
+            console.log("hello2");
+            }
+        });
+
+ //.........................................................................POPOVER
+
+    $('[data-toggle="popover"]').popover(); 
+
+
+
+});
